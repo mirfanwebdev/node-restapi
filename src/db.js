c8a0683a19aabd3b959a8f073423c28d0d38cd23
m.irfan <m.irfanwebdev@gmail.com>
2025-11-21 20:11:31 +0700

    fix file encoding to utf-8

--- src/db.js
+++ src/db.js
@@ -1,13 +1,33 @@
 const db = {
-	users: [
-	{id: "1". name:"John Doe", email:"john@example.com", password: "password123"},
-	{id: "2". name:"Jane Smith", email:"jane@example.com", password: "secretpass"}
-	],
-	posts: [
-	{id: "1", userId: "1", title: "Helo Node", content: "My first post!"},
-	{id: "2", userId: "1", title: "Refactoring", content: "Makes code better."},
-	{id: "3", userId: "1", title: "New Post", content: "Checking git history"}
-	]
-}
+  users: [
+    {
+      id: "1",
+      name: "John Doe",
+      email: "john@example.com",
+      password: "password123",
+    },
+    {
+      id: "2",
+      name: "Jane Smith",
+      email: "jane@example.com",
+      password: "secretpass",
+    },
+  ],
+  posts: [
+    { id: "1", userId: "1", title: "Helo Node", content: "My first post!" },
+    {
+      id: "2",
+      userId: "1",
+      title: "Refactoring",
+      content: "Makes code better.",
+    },
+    {
+      id: "3",
+      userId: "1",
+      title: "New Post",
+      content: "Checking git history",
+    },
+  ],
+};
 
 export default db;
